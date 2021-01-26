fd326a2aee1eb80f5824572e6345dbe5b399c79f
ms123myrddin <47523025+ms123myrddin@users.noreply.github.com>
2021-01-26 15:37:26 +0100

    Create index.php

---
+++ home/index.php
@@ -0,0 +1,18 @@
+    <div class="Container Container--spaced">
+      <div id="projects" class="Phones Phones--three">
+        <div class="Phone"><a target="_blank" href="https://www.kinechristinegodefroid.com/" >Kine christine godefroid</a><img src="./img/kine.png"/></div>
+        <div class="Phone"><a target="_blank" href="https://restaurantvillaborghese.be/" >villa borghese</a><img src="./img/villa.jpg"/></div>
+        <div class="Phone"><p>this may be yours</p><img class="yours" src="./img/logo.png" /><a class="contact"href="mailto:contact.mier.dev@gmail.com">contact</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="Features">
+    <div class="Container">
+      <div class="Feature"></div>
+      <div class="Feature"></div>
+      <div class="Feature"></div>
+    </div>
+  </div>
+  <div class="Footer">
+    <div class="Container"></div>
+  </div>
